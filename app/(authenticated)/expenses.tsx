8ad2778115fc08ea24979eed612b7bdfb3da8889
Rohit Kumar Saini <40729749+rockingrohit9639@users.noreply.github.com>
2023-12-20 10:36:38 +0530

    Feature/deployment (#19)
    * feat(build): setup build files
    
    * refactor(auth): update signup route
    
    * fix(build): add env variables in all build types
    
    * ci(version): bump version

--- app/(authenticated)/expenses.tsx
+++ app/(authenticated)/expenses.tsx
@@ -1,12 +1,10 @@
-import { AddIcon, Button, Heading, VStack } from 'native-base'
+import { AddIcon, Button, VStack } from 'native-base'
 import { Link } from 'expo-router'
 import ExpenseList from '@/components/expense-list'
 
 export default function Expenses() {
   return (
     <VStack flex={1} p={4} space={4}>
-      <Heading>Expenses</Heading>
-
       <Link href="/create-expense" asChild>
         <Button startIcon={<AddIcon />}>Create Expense</Button>
       </Link>
