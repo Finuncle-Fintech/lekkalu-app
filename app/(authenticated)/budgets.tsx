3e54e377b05d807db61d926c2efc2183d0f930d4
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-01-12 21:54:13 +0530

    feat(budgets): showing list of budgets

--- app/(authenticated)/budgets.tsx
+++ app/(authenticated)/budgets.tsx
@@ -1,12 +1,46 @@
-import { Text, View } from 'tamagui'
+import { Heading, ScrollView, Text, View, YStack } from 'tamagui'
+import { useQuery } from '@tanstack/react-query'
+import { range } from 'lodash'
+import dayjs from 'dayjs'
 import SetBudget from '@/screen-components/set-budget'
+import { fetchBudgets } from '@/queries/budget'
+import { MONTH_COLOR_MAP } from '@/utils/budget'
 
 export default function Budgets() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['BUDGETS'],
+    queryFn: fetchBudgets,
+  })
+
+  if (isLoading) {
+    return (
+      <YStack flex={1} bg="$background" p="$4" space="$4">
+        {range(5).map((i) => (
+          <View key={i} h="$10" width="100%" bg="$backgroundHover" borderRadius="$5" />
+        ))}
+      </YStack>
+    )
+  }
+
   return (
     <View flex={1} bg="$background" p="$4">
       <SetBudget />
 
-      <Text>Testing</Text>
+      <ScrollView mt="$4" space="$4">
+        {data?.map((budget) => (
+          <YStack
+            key={budget.id}
+            p="$4"
+            borderWidth="$1"
+            borderRadius="$5"
+            space="$2"
+            backgroundColor={MONTH_COLOR_MAP[dayjs(budget.month).month()]}
+          >
+            <Heading fontWeight="bold">{dayjs(budget.month).format('MMMM')}</Heading>
+            <Text fontSize="$5">₹ {budget.limit}</Text>
+          </YStack>
+        ))}
+      </ScrollView>
     </View>
   )
 }
