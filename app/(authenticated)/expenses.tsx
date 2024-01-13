c3e73c923b3a3c1235d5d72d9fd65e4d10df69eb
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-01-13 14:03:24 +0530

    feat(expenses): add floating action button to create new expense

--- app/(authenticated)/expenses.tsx
+++ app/(authenticated)/expenses.tsx
@@ -1,18 +1,23 @@
 import { useQuery } from '@tanstack/react-query'
 import dayjs from 'dayjs'
-import { Button, Text, XStack, YStack } from 'tamagui'
-import { Link } from 'expo-router'
-import { ArrowRight } from '@tamagui/lucide-icons'
+import { Button, Text, XStack, YStack, useTheme } from 'tamagui'
+import { Link, useRouter } from 'expo-router'
+import { ArrowRight, Plus } from '@tamagui/lucide-icons'
+import { FloatingAction } from 'react-native-floating-action'
 import ExpenseList from '@/components/expense-list'
 import { BUDGET_QUERY_KEYS } from '@/utils/query-keys'
 import { fetchBudgets } from '@/queries/budget'
 import SetBudget from '@/screen-components/set-budget'
 
 export default function Expenses() {
+  const theme = useTheme()
+
   const { data: budgets, isLoading } = useQuery({
     queryKey: [BUDGET_QUERY_KEYS.BUDGETS],
     queryFn: fetchBudgets,
   })
+
+  const router = useRouter()
 
   const currentMonthBudget = budgets?.find((budget) => dayjs(budget.month).month() === dayjs().month())
 
@@ -40,11 +45,24 @@ export default function Expenses() {
         </YStack>
       )}
 
-      {/* <Link href="/create-expense" asChild>
-        <Button startIcon={<AddIcon />}>Create Expense</Button>
-      </Link> */}
-
       <ExpenseList />
+
+      <FloatingAction
+        color={theme.primary.get()}
+        actions={[
+          {
+            text: 'Add New Expense',
+            name: 'add_expense',
+            icon: <Plus />,
+            color: theme.primary.get(),
+          },
+        ]}
+        onPressItem={(name) => {
+          if (name === 'add_expense') {
+            router.push('/create-expense')
+          }
+        }}
+      />
     </YStack>
   )
 }
