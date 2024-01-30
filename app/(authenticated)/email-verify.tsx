fcacbaef6d5baf936cb89ce428f0a8b905c87376
Snehal Dhameliya <snehaldhameliya17@gmail.com>
2024-01-31 00:04:37 +0530

    implement verify email feature

---
+++ app/(authenticated)/email-verify.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Text, View, Button } from 'tamagui'
+import { useForm } from 'react-hook-form'
+import { router } from 'expo-router'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { VerifyEmailSchema, verifyEmailSchema } from '@/schema/settings'
+import { hp, wp } from '@/utils/responsive'
+import LoaderOverlay from '@/components/loader-overlay'
+import BackButton from '@/components/back-button'
+import { FontSizes } from '@/utils/fonts'
+import KeyboardScrollView from '@/components/keyboard-scroll-view'
+import InputFields from '@/components/input-fields'
+import { VERIFY_EMAIL_FIELDS } from '@/utils/settings'
+import { useResetPassword } from '@/queries/settings'
+import { openDeviceMailClientApp } from '@/utils/helpers'
+
+const EmailVerify = () => {
+  const insets = useSafeAreaInsets()
+  const { control, handleSubmit, formState, reset } = useForm<VerifyEmailSchema>({
+    resolver: zodResolver(verifyEmailSchema),
+    defaultValues: { email: '' },
+  })
+  const { mutate, status } = useResetPassword(reset)
+
+  const handleSendLink = (values: VerifyEmailSchema) => {
+    mutate({ email: values.email })
+  }
+
+  const isSuccess = status === 'success'
+  const isPending = status === 'pending'
+
+  return (
+    <View f={1} pt={insets.top + hp(2)} bg="$backgroundHover">
+      {!!isPending && <LoaderOverlay />}
+      <View fd="row" ai="center" columnGap={wp(4)} mx={wp(5)}>
+        <BackButton onPress={() => router.push('/(authenticated)/settings')} />
+        <Text fontSize={FontSizes.size20} fontFamily={'$heading'}>
+          {'Verify Email'}
+        </Text>
+      </View>
+
+      {isSuccess ? (
+        <View px={wp(5)} mt={hp(2)} rowGap={hp(1.5)}>
+          <Text color={'$mutedForeground'} fontSize={FontSizes.size16} mt={hp(2)} lh={hp(3)} fontFamily={'$heading'}>
+            {'We have sent an mail to the email. Please open your inbox'}
+          </Text>
+          <Button
+            fontSize={FontSizes.size18}
+            h={hp(5.5)}
+            onPress={openDeviceMailClientApp}
+            bg="$primary"
+            color="white"
+            mt={hp(2)}
+          >
+            Open Mail App
+          </Button>
+        </View>
+      ) : (
+        <KeyboardScrollView contentContainerStyle={styles.scrollContent}>
+          <Text color={'$mutedForeground'} fontSize={FontSizes.size16} mt={hp(2)} lh={hp(3)} fontFamily={'$heading'}>
+            {"We'll send an verification link to the entered email to verify it"}
+          </Text>
+          <InputFields control={control} errors={formState.errors} inputs={VERIFY_EMAIL_FIELDS} />
+          <Button
+            fontSize={FontSizes.size18}
+            h={hp(5.5)}
+            onPress={handleSubmit(handleSendLink)}
+            bg="$primary"
+            color="white"
+            mt={hp(2)}
+          >
+            Send Link
+          </Button>
+        </KeyboardScrollView>
+      )}
+    </View>
+  )
+}
+
+export default EmailVerify
+
+const styles = StyleSheet.create({
+  scrollContent: {
+    paddingHorizontal: wp(5),
+    marginTop: hp(2),
+    rowGap: hp(1.5),
+    paddingBottom: hp(5),
+  },
+})
--- app/(authenticated)/reset-password.tsx
+++ app/(authenticated)/reset-password.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { Linking, StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native'
 import { Text, View, Button } from 'tamagui'
 import { useForm } from 'react-hook-form'
 import { router, useLocalSearchParams } from 'expo-router'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { IntentLauncherParams, startActivityAsync } from 'expo-intent-launcher'
-import { useToast } from 'native-base'
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ChangePasswordSchema, changePasswordSchema } from '@/schema/settings'
-import { hp, isAndroid, isIOS, wp } from '@/utils/responsive'
+import { hp, wp } from '@/utils/responsive'
 import LoaderOverlay from '@/components/loader-overlay'
 import BackButton from '@/components/back-button'
 import { FontSizes } from '@/utils/fonts'
@@ -18,11 +16,11 @@ import InputFields from '@/components/input-fields'
 import { RESET_PASSWORD_FIELDS } from '@/utils/settings'
 import { useResetPassword } from '@/queries/settings'
 import { useGetUserDetails } from '@/queries/auth'
+import { openDeviceMailClientApp } from '@/utils/helpers'
 
 export default function ResetPassword() {
   const insets = useSafeAreaInsets()
   const params = useLocalSearchParams()
-  const toast = useToast()
   const isForgotPassword = !!params?.isForgotPassword
   const { data: userData } = useGetUserDetails()
   const { control, handleSubmit, formState, reset } = useForm<ChangePasswordSchema>({
@@ -35,44 +33,6 @@ export default function ResetPassword() {
 
   const handleSendLink = (values: ChangePasswordSchema) => {
     mutate({ email: values.email })
-  }
-
-  const handleOpenMailClientError = (e?: any) => {
-    console.log('🚀 ~ handleOpenMailClientError ~ e:', e)
-    toast.show({
-      title: 'Failed to open mail app! Please open it manually',
-    })
-  }
-
-  const openMailClientIOS = () => {
-    Linking.canOpenURL('message:0')
-      .then((supported) => {
-        if (!supported) {
-          handleOpenMailClientError()
-        } else {
-          return Linking.openURL('message:0').catch(handleOpenMailClientError)
-        }
-      })
-      .catch(handleOpenMailClientError)
-  }
-
-  const openMailClientAndroid = () => {
-    const activityAction = 'android.intent.action.MAIN' // Intent.ACTION_MAIN
-    const intentParams: IntentLauncherParams = {
-      flags: 268435456, // Intent.FLAG_ACTIVITY_NEW_TASK
-      category: 'android.intent.category.APP_EMAIL', // Intent.CATEGORY_APP_EMAIL
-    }
-
-    startActivityAsync(activityAction, intentParams).catch(handleOpenMailClientError)
-  }
-
-  const handleOpenMailApp = () => {
-    if (isIOS) {
-      openMailClientIOS()
-    }
-    if (isAndroid) {
-      openMailClientAndroid()
-    }
   }
 
   const isSuccess = status === 'success'
@@ -96,7 +56,7 @@ export default function ResetPassword() {
           <Button
             fontSize={FontSizes.size18}
             h={hp(5.5)}
-            onPress={handleOpenMailApp}
+            onPress={openDeviceMailClientApp}
             bg="$primary"
             color="white"
             mt={hp(2)}
