16d1c76801205223dd576cce662a0732d00fcad6
Snehal Dhameliya <snehaldhameliya17@gmail.com>
2024-01-31 00:04:37 +0530

    implement verify email feature

--- queries/settings.ts
+++ queries/settings.ts
@@ -6,6 +6,7 @@ import { router } from 'expo-router'
 import { FeedbackSchema } from '@/schema/settings'
 import { BASE_URL, apiClient, userClient } from '@/utils/client'
 import { getAxiosHeaderWithToken } from '@/utils/helpers'
+import axios from 'axios'
 
 interface SubmitFeedbackResponseType {
   id: number
