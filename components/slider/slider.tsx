e51fb681cc1fd829be3ba90d8d059fdb467dca8d
Hardik <talaviyahardik4@gmail.com>
2024-02-19 14:16:24 +0530

    Slider Changes

--- components/slider/slider.tsx
+++ components/slider/slider.tsx
@@ -1,6 +1,5 @@
-import Slider from '@react-native-community/slider'
 import { Box, VStack, View } from 'native-base'
-// import { Slider } from 'tamagui'
+import { Slider } from 'tamagui'
 
 type SliderProps = {
   defaultValue?: number,
@@ -14,18 +13,12 @@ export default function CommoneSlider({ defaultValue, maxValue, minValue, onChan
     <View alignItems="center" justifyContent="center">
       <Box alignItems="center" w="100%">
         <VStack space={4} w="100%" maxW="300">
-
-        <Slider
-            value={defaultValue}
-            step={1}
-            style={{ width: "100%", height: 34 }}
-            minimumValue={minValue}
-            maximumValue={maxValue}
-            minimumTrackTintColor="#003562"
-            maximumTrackTintColor="#e1e1e1"
-            thumbTintColor='#003562'
-            onValueChange={(val)=>onChange(parseInt(val))}
-          />
+          <Slider size="$1" width={"100%"} defaultValue={[defaultValue]} max={maxValue} min={minValue} step={1} onValueChange={(val)=>onChange(parseInt(val))} hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}>
+    <Slider.Track style={{ backgroundColor: "#e1e1e1" }}>
+      <Slider.TrackActive style={{ backgroundColor: "#003562" }}/>
+    </Slider.Track>
+    <Slider.Thumb circular index={0} style={{ backgroundColor: "#003562", borderColor: "#003562" }}/>
+  </Slider>
         </VStack>
       </Box>
     </View>
