840740abf5d8f07058407c0007baeb36954f513b
Sandip Satyal <sandipsatyal25@gmail.com>
2024-02-20 15:08:51 +0545

    add: Search bar and calendar icon to search by text and date.

--- components/expense-list/expense-list.tsx
+++ components/expense-list/expense-list.tsx
@@ -1,18 +1,22 @@
 import { useQueries } from '@tanstack/react-query'
-import { Button, FlatList, HStack, IconButton, Text, VStack } from 'native-base'
-import { useCallback } from 'react'
+import { Button, FlatList, HStack, IconButton, Text, VStack, Modal } from 'native-base'
+import { useCallback, useState } from 'react'
+// import { Modal } from 'react-native'
 import { EvilIcons } from '@expo/vector-icons'
 import { Link } from 'expo-router'
-import { View, useTheme } from 'tamagui'
+import { Input, View, useTheme } from 'tamagui'
+import { Calendar, Search, X } from '@tamagui/lucide-icons'
 import { EXPENSES, TAGS } from '@/utils/query-keys'
 import { fetchExpenses } from '@/queries/expense'
 import { fetchTags } from '@/queries/tag'
 import Loading from '../loading'
 import DeleteExpense from '../delete-expense'
 import { FontSizes } from '@/utils/fonts'
+import DatePicker from '../date-picker/date-picker'
 
 export default function ExpenseList() {
   const theme = useTheme()
+  const [modalOpen, setModalOpen] = useState(false)
 
   const [expenseQuery, tagsQuery] = useQueries({
     queries: [
@@ -44,56 +48,101 @@ export default function ExpenseList() {
   }
 
   return (
-    <FlatList
-      refreshing={expenseQuery.isLoading || tagsQuery.isLoading}
-      keyExtractor={(item) => item.id.toString()}
-      data={expenseQuery.data ?? []}
-      renderItem={({ item }) => (
-        <VStack
-          space={4}
-          bg={theme.background.get()}
-          rounded="md"
-          p="4"
-          shadow="sm"
-          mb="4"
-          display={'flex'}
-          flexDirection={'row'}
-          justifyContent={'space-between'}
+    <>
+      <View marginBottom={20} display="flex" flexDirection="row" justifyContent={'space-between'}>
+        <View width={'80%'}>
+          <Input placeholder="Search Expenses" />
+          <Search style={{ position: 'absolute', right: 15, top: 10 }} />
+        </View>
+        <View>
+          <Button onPress={() => setModalOpen(true)}>
+            <Calendar />
+          </Button>
+        </View>
+      </View>
+      <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)} animationPreset="slide">
+        <View
+          backgroundColor={'$backgroundHover'}
+          height={'40%'}
+          position="absolute"
+          bottom={0}
+          width={'100%'}
+          padding={20}
         >
-          <View width={'$18'}>
-            <HStack space={2}>
-              <Text color={theme.foreground.get()} fontSize={FontSizes.size20}>
-                Amount :{' '}
+          <View>
+            <View display="flex" flexDirection="row" justifyContent="space-between">
+              <Text color={'white'} fontSize={FontSizes.size20}>
+                Search Expenses by Date
               </Text>
-              <Text color={theme.foreground.get()} fontSize={FontSizes.size20} fontWeight="bold">
-                {item.amount}
-              </Text>
-            </HStack>
-            <HStack space={2} display={'flex'} flexWrap={'wrap'}>
-              <Text color={theme.foreground.get()} fontSize={FontSizes.size20}>
-                Tags :{' '}
-              </Text>
-              <Text color={theme.foreground.get()} fontSize={FontSizes.size20} fontWeight="bold">
-                {getTagNames(item.tags)}
-              </Text>
-            </HStack>
+              <View>
+                <X onPress={() => setModalOpen(false)} />
+              </View>
+            </View>
           </View>
-          <Button.Group display={'flex'} alignItems={'center'}>
-            <DeleteExpense id={item.id} />
-            <Link href={`/update-expense/${item.id}`} asChild>
-              <IconButton
-                size={10}
-                variant="solid"
-                _icon={{
-                  as: EvilIcons,
-                  name: 'pencil',
-                  size: 8,
-                }}
-              />
-            </Link>
-          </Button.Group>
-        </VStack>
-      )}
-    />
+          <View display="flex" flexDirection="column" gap={20} marginTop={20}>
+            <DatePicker placeholder="From" />
+            <DatePicker placeholder="To" />
+          </View>
+          <View display="flex" gap={20} marginTop={20}>
+            <Button>Search</Button>
+            <Button background={'danger.700'}>Reset</Button>
+          </View>
+        </View>
+      </Modal>
+      <FlatList
+        refreshing={expenseQuery.isLoading || tagsQuery.isLoading}
+        keyExtractor={(item) => item.id.toString()}
+        data={expenseQuery.data ?? []}
+        renderItem={({ item }) => (
+          <VStack
+            space={4}
+            bg={theme.background.get()}
+            rounded="md"
+            p="4"
+            shadow="sm"
+            mb="4"
+            display={'flex'}
+            flexDirection={'row'}
+            justifyContent={'space-between'}
+          >
+            <View width={'$18'}>
+              <HStack space={2}>
+                <Text color={theme.foreground.get()} fontSize={FontSizes.size20}>
+                  Amount :{' '}
+                </Text>
+                <Text color={theme.foreground.get()} fontSize={FontSizes.size20} fontWeight="bold">
+                  {item.amount}
+                </Text>
+              </HStack>
+              <HStack space={2} display={'flex'} flexWrap={'wrap'}>
+                <Text color={theme.foreground.get()} fontSize={FontSizes.size20}>
+                  Tags :{' '}
+                </Text>
+                <Text color={theme.foreground.get()} fontSize={FontSizes.size20} fontWeight="bold">
+                  {getTagNames(item.tags)}
+                </Text>
+              </HStack>
+            </View>
+            <Button.Group display={'flex'} alignItems={'center'}>
+              <DeleteExpense id={item.id} />
+              <Link href={`/update-expense/${item.id}`} asChild>
+                <IconButton
+                  size={10}
+                  variant="solid"
+                  _icon={{
+                    as: EvilIcons,
+                    name: 'pencil',
+                    size: 8,
+                  }}
+                />
+              </Link>
+            </Button.Group>
+          </VStack>
+        )}
+      />
+      <View>
+        <Text color={'red.100'}>Pagination here</Text>
+      </View>
+    </>
   )
 }
