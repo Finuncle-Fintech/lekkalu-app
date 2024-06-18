608285c1871d0b3a99d0ff50272ee66f48d009ab
Sandip Satyal <sandipsatyal25@gmail.com>
2024-06-18 18:10:28 +0545

    feat: allow user to delete scenario entities.

--- components/scenarios/Entity/ListEntity.tsx
+++ components/scenarios/Entity/ListEntity.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, useTheme } from 'tamagui'
 import { FlatList } from 'native-base'
 import { ScenarioEntities } from '@/app/(authenticated)/scenarios/[id]'
 import { wp } from '@/utils/responsive'
+import EditDeleteMenu from '@/components/edit-delete-menu/edit-delete-menu'
+import useScenario from '@/hooks/use-scenario'
 
 type ListEntityType = {
   data: any
@@ -12,6 +14,10 @@ type ListEntityType = {
 
 const ListEntity = ({ data, isLoading, refetch }: ListEntityType) => {
   const theme = useTheme()
+  const { deleteLiabilityMutation, deleteIncomeExpenseMutation, deletePhysicalAssetMutation } = useScenario()
+  const { mutate: deleteExpense, isSuccess: deletedExpense } = deleteIncomeExpenseMutation
+  const { mutate: deleteLiabilities, isSuccess: deletedLiabilities } = deleteLiabilityMutation
+  const { mutate: deleteAsset, isSuccess: deletedAsset } = deletePhysicalAssetMutation
   const getTextColor = (type: ScenarioEntities) => {
     switch (type) {
       case 'Asset':
@@ -22,6 +28,29 @@ const ListEntity = ({ data, isLoading, refetch }: ListEntityType) => {
         return '$blue10'
     }
   }
+
+  const handleDelete = (entity_type: string, id: number) => {
+    switch (entity_type) {
+      case 'Expense':
+        deleteExpense(id)
+        break
+      case 'Asset':
+        deleteAsset(id)
+        break
+      case 'Liabilities':
+        deleteLiabilities(id)
+        break
+      default:
+        break
+    }
+  }
+
+  useEffect(() => {
+    if (deletedAsset || deletedExpense || deletedLiabilities) {
+      refetch()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [deletedAsset, deletedExpense, deletedLiabilities])
 
   return (
     <FlatList
@@ -50,18 +79,24 @@ const ListEntity = ({ data, isLoading, refetch }: ListEntityType) => {
             bg={theme.background.get()}
             m={5}
             key={item?.id}
-            borderRadius={5}
+            borderRadius={wp(2)}
             elevationAndroid={4}
             shadowOffset={{ height: 0, width: 0 }}
             shadowColor={'black'}
             shadowOpacity={0.1}
             shadowRadius={wp(1)}
+            flexDirection="row"
           >
-            <Text fontSize={'$1'} mb={3} color={getTextColor(item?.entity_type)}>
-              {item?.entity_type}
-            </Text>
-            <Text fontSize={'$7'}>{item?.name || ''}</Text>
-            <Text pt={8}>Rs. {item?.amount}</Text>
+            <View w="90%">
+              <Text fontSize={'$1'} mb={3} color={getTextColor(item?.entity_type)}>
+                {item?.entity_type}
+              </Text>
+              <Text fontSize={'$7'}>{item?.name || ''}</Text>
+              <Text pt={8}>Rs. {item?.amount}</Text>
+            </View>
+            <View alignSelf="center">
+              <EditDeleteMenu onDelete={() => handleDelete(item?.entity_type, item?.id)} />
+            </View>
           </View>
         )
       }}
