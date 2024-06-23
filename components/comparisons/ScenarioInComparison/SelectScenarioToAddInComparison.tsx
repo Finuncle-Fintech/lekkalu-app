82e8863d8a144d95b3f74fbf78d4f9342524e1bc
Sandip Satyal <sandipsatyal25@gmail.com>
2024-06-24 00:06:49 +0545

    fix: add scenario modal in comparison add screen.

--- components/comparisons/ScenarioInComparison/SelectScenarioToAddInComparison.tsx
+++ components/comparisons/ScenarioInComparison/SelectScenarioToAddInComparison.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TouchableOpacity } from 'react-native'
+import { TouchableOpacity, useColorScheme } from 'react-native'
 import { View, Text } from 'tamagui'
 import { CircleDashed, Lock, CheckCircle2 } from '@tamagui/lucide-icons'
 import { FontSizes } from '@/utils/fonts'
@@ -19,13 +19,34 @@ const SelectScenarioToAddInComparison = ({
   onPress,
   id,
 }: SelectScenarioToAddInComparisonType) => {
+  const systemTheme = useColorScheme()
+  const background = systemTheme === 'dark' ? 'black' : 'white'
+  const textColor = systemTheme === 'dark' ? 'white' : 'black'
   return (
-    <TouchableOpacity style={{ flexDirection: 'row' }} disabled={isPrivate} onPress={() => onPress(id)}>
-      <View>{isSelected ? <CheckCircle2 /> : <CircleDashed />}</View>
-      <View>
-        <Text fontSize={FontSizes.size18}>{name}</Text>
+    <TouchableOpacity
+      style={{
+        flexDirection: 'row',
+        backgroundColor: background,
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+        borderRadius: 10,
+      }}
+      disabled={isPrivate}
+      onPress={() => onPress(id)}
+    >
+      <View alignSelf="center">
+        {isSelected ? (
+          <CheckCircle2 color={isPrivate ? '$gray10' : textColor} />
+        ) : (
+          <CircleDashed color={isPrivate ? '$gray10' : textColor} />
+        )}
       </View>
-      <View>{isPrivate ? <Lock /> : <></>}</View>
+      <View px={'$3'}>
+        <Text fontSize={FontSizes.size15} color={isPrivate ? '$gray10' : textColor}>
+          {name}
+        </Text>
+      </View>
+      <View ml={'auto'}>{isPrivate ? <Lock color={'$red10Dark'} /> : <></>}</View>
     </TouchableOpacity>
   )
 }
