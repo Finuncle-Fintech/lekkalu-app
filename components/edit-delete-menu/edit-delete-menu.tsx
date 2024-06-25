3aa94b1cffe79f34619e0a12b1f49caa45b32bb0
Sandip Satyal <sandipsatyal25@gmail.com>
2024-06-25 18:48:36 +0545

    fix: add dialog to ask for confirmation in delete menu.

--- components/edit-delete-menu/edit-delete-menu.tsx
+++ components/edit-delete-menu/edit-delete-menu.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { TouchableOpacity } from 'react-native'
-import { useTheme } from 'tamagui'
+import { Modal } from 'native-base'
+import { View, useTheme, Text, Button } from 'tamagui'
 import { Menu, MenuItem } from 'react-native-material-menu'
 import { Feather } from '@expo/vector-icons'
 import { wp } from '@/utils/responsive'
@@ -17,6 +18,7 @@ const EditDeleteMenu: FC<EditDeleteMenuProps> = (props) => {
 
   const theme = useTheme()
   const [visible, setVisible] = useState(false)
+  const [viewDeleteDialog, setViewDeleteDialog] = useState(false)
 
   const showMenu = () => {
     setVisible(true)
@@ -31,37 +33,75 @@ const EditDeleteMenu: FC<EditDeleteMenuProps> = (props) => {
     hideMenu()
   }
 
+  useEffect(() => {
+    return () => {
+      setViewDeleteDialog(false)
+    }
+  }, [])
+
   const handleDeletePress = () => {
     onDelete?.()
     hideMenu()
   }
   return (
-    <Menu
-      visible={visible}
-      anchor={
-        <TouchableOpacity onPress={showMenu}>
-          <Feather size={wp(4)} name="more-vertical" color={theme.foreground.get()} />
-        </TouchableOpacity>
-      }
-      onRequestClose={hideMenu}
-      style={{ backgroundColor: theme.backgroundHover.get() }}
-    >
-      <MenuItem textStyle={{ color: theme.foreground.get(), fontSize: FontSizes.size16 }} onPress={handleEditPress}>
-        Edit
-      </MenuItem>
-      <MenuItem textStyle={{ color: 'red', fontSize: FontSizes.size16 }} onPress={handleDeletePress}>
-        Delete
-      </MenuItem>
-      {props.extraMenus ? (
-        props.extraMenus?.map((each) => (
-          <MenuItem key={each?.name} textStyle={each?.style} onPress={each?.onPress}>
-            {each?.name}
-          </MenuItem>
-        ))
-      ) : (
-        <></>
-      )}
-    </Menu>
+    <>
+      <Menu
+        visible={visible}
+        anchor={
+          <TouchableOpacity onPress={showMenu}>
+            <Feather size={wp(4)} name="more-vertical" color={theme.foreground.get()} />
+          </TouchableOpacity>
+        }
+        onRequestClose={hideMenu}
+        style={{ backgroundColor: theme.backgroundHover.get() }}
+      >
+        <MenuItem textStyle={{ color: theme.foreground.get(), fontSize: FontSizes.size16 }} onPress={handleEditPress}>
+          Edit
+        </MenuItem>
+        <MenuItem
+          textStyle={{ color: 'red', fontSize: FontSizes.size16 }}
+          onPress={() => {
+            setViewDeleteDialog(true)
+            setVisible(false)
+          }}
+        >
+          Delete
+        </MenuItem>
+        {props.extraMenus ? (
+          props.extraMenus?.map((each) => (
+            <MenuItem key={each?.name} textStyle={each?.style} onPress={each?.onPress}>
+              {each?.name}
+            </MenuItem>
+          ))
+        ) : (
+          <></>
+        )}
+      </Menu>
+      <Modal isOpen={viewDeleteDialog} onClose={() => setViewDeleteDialog(false)}>
+        <Modal.Content>
+          <Modal.Header>
+            <View>
+              <Text color={theme.foreground.get()}>Delete</Text>
+            </View>
+          </Modal.Header>
+          <Modal.Body>
+            <View>
+              <Text color={theme.foreground.get()}>Are you sure you want to delete this?</Text>
+            </View>
+          </Modal.Body>
+          <Modal.Footer>
+            <View f={1} fd={'row'} gap={'$5'}>
+              <Button onPress={() => setViewDeleteDialog(false)} f={1}>
+                Cancel
+              </Button>
+              <Button onPress={handleDeletePress} backgroundColor={'$primary'} f={1}>
+                Confirm
+              </Button>
+            </View>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
+    </>
   )
 }
 
