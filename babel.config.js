dc65a9dd97bb1c43b4b56dd2fc1f016e50dbd459
Sandip Satyal <sandipsatyal25@gmail.com>
2024-08-11 18:21:57 +0545

    upgrade: Expo SDK from 49 to 51.

--- babel.config.js
+++ babel.config.js
@@ -13,7 +13,6 @@ module.exports = function (api) {
           logTimings: true,
         },
       ],
-      'expo-router/babel',
       'react-native-reanimated/plugin',
       [
         'module-resolver',
